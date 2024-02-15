75f5284b1c714e6d2bc69298cb8d532130314f12
Replit user <>
2024-02-15 17:03:17 +0000

    Error unix

--- index.js
+++ index.js
@@ -37,9 +37,14 @@ app.get("/api/:date", function (req, res) {
 });
 
 app.get("/api", (req, res) => {
+  let date = new Date();
+  let UTC = date.getTime();
+  UTC = new Date(UTC);
+  UTS = UTC.toUTCString();
+  let UNIX = date.getTime();
   res.json({
-    unix: new Date().getTime(),
-    utc: new Date().toUTCString(),
+    unix: UNIX,
+    utc: UTS,
   });
 });
 
